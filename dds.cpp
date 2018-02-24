9b0121ffc46447e658a32a53a7ed0bae8b092dcb
Vladimir Kirievskiy <vek2006_73@mail.ru>
2018-02-24 16:18:08 +0300

    sweeping

--- dds.cpp
+++ dds.cpp
@@ -5,7 +5,7 @@ AD9833 gen(10);
 void vInitDDS() {
 	gen.Begin();
 	gen.EnableOutput(false);
-	gen.ApplySignal(SINE_WAVE, REG0, 1000);
+	gen.ApplySignal(SINE_WAVE, REG0, FREQ_BASIC_INIT_VALUE_HZ);
 	gen.SetOutputSource(REG0);
 	gen.EnableOutput(true);
 
@@ -13,6 +13,43 @@ void vInitDDS() {
 }
 
 void vDDSset() {
+	
+	WaveformType xWaveformType;
+
+	switch (cMode) {
+		case MODE_SIN: 
+			xWaveformType = SINE_WAVE;
+			break;
+		case MODE_PULSE:
+			xWaveformType = SQUARE_WAVE;
+			break;
+		case MODE_TRIANGLE:
+			xWaveformType = TRIANGLE_WAVE;
+			break;
+		case MODE_SWEEP:
+			xWaveformType = SINE_WAVE;
+			break;
+		default:
+			xWaveformType = SINE_WAVE;
+	}
+	
+	if (cMode!= MODE_SWEEP) {
+		gen.EnableOutput(true);
+		gen.ApplySignal(xWaveformType, REG0, lFreqBasicHz);
+	}
+	else { //2do: sweeping		
+		
+		unsigned long ulStepSweep = (lFreqBasicHz - lFreqLowHz) / SWEEP_DISCRETE;
+		gen.EnableOutput(true);
+
+		gen.ApplySignal(SINE_WAVE, REG0, lFreqLowHz);
+
+		for (int i = 0; i < SWEEP_DISCRETE; i++) {
+			gen.IncrementFrequency(REG0, ulStepSweep);
+			delayMicroseconds (SWEEP_DELAY_US);
+		}
+		gen.EnableOutput(false);
+	}
 
 }
 
--- dds.h
+++ dds.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "globals.h"
-#include <AD9833.h> 
+#include "AD9833.h" 
+#include "avdweb_VirtualDelay.h"
 
 void vInitDDS();
 void vDDSset();
